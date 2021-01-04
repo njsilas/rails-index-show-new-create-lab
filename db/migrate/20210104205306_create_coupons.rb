c3ce32c12554636ac1af979b45813c3736053751
Nicholas Silas <njsilas@gmail.com>
2021-01-04 16:37:50 -0500

    commit tree 32b2f75e57d0b0941b7f6223461ceffa60cca388

---
+++ db/migrate/20210104205306_create_coupons.rb
@@ -0,0 +1,12 @@
+class CreateCoupons < ActiveRecord::Migration[5.0]
+  def change
+    create_table :coupons do |t|
+      t.string :coupon_code
+      t.string :string
+      t.string :store
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/coupons.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :coupon do
+    coupon_code { "MyString" }
+    string { "MyString" }
+    store { "MyString" }
+    string { "MyString" }
+  end
+end
